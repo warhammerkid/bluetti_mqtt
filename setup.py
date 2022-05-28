189426ad34d4525de4337301801f62874b0842ae
Stephen Augenstein <perl.programmer@gmail.com>
2022-05-28 18:29:38 -0400

    Modernize setuptools config

--- setup.py
+++ setup.py
@@ -1,21 +1,4 @@
 from setuptools import setup
 
 
-setup(
-    name='bluetti_mqtt',
-    version='0.1.1',
-    description='MQTT interface to Bluetti power stations',
-    url='http://github.com/warhammerkid/bluetti_mqtt',
-    author='Stephen Augenstein',
-    author_email='perl.programmer@gmail.com',
-    license='MIT',
-    packages=['bluetti_mqtt'],
-    python_requires='>=3.7',
-    install_requires=[
-        'bleak',
-        'crcmod',
-    ],
-    entry_points={
-        'console_scripts': ['bluetti-logger=bluetti_mqtt.logger_cli:main']
-    },
-    zip_safe=False)
+setup()
