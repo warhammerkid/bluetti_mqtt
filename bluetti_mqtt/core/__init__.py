3de2b64d44aeada8ea279d79d75cca1ba0c5beb9
Brian <brimue@gmail.com>
2023-12-13 17:56:59 -0500

    Add AC70 support

--- bluetti_mqtt/core/__init__.py
+++ bluetti_mqtt/core/__init__.py
@@ -3,6 +3,7 @@ from .devices.ac200m import AC200M
 from .devices.ac300 import AC300
 from .devices.ac500 import AC500
 from .devices.ac60 import AC60
+from .devices.ac70 import AC70
 from .devices.ep500 import EP500
 from .devices.ep500p import EP500P
 from .devices.ep600 import EP600
